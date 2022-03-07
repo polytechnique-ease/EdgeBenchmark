f2ee7afa1aebda08c2dbcae529039e196de47c3c
mahsahadian <mahsa_hadian@yahoo.com>
2022-03-07 10:23:55 -0500

    spark docker-compose

--- Edge Side/monitoring.py
+++ Edge Side/monitoring.py
@@ -140,7 +140,7 @@ class DockerMonitoring():
                                   'net_tx': float(self.get_network_throughput(container_stats)['tx'])}
                 json_body = [
                               {
-                              "measurement": "tt_bl_4_5min",
+                              "measurement": "test_spark_MSR",
                               "tags": {
                               "container_name": name,
                               "short_id": cont.short_id
@@ -178,7 +178,7 @@ class DockerMonitoring():
         
         json_body = [
              {
-              "measurement": "tt_bl_4_5min",
+              "measurement": "test_spark_MSR",
                "tags": {
                 "host": "rpi",
                 "region": "us-west"
