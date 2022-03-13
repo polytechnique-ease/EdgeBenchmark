68ce55d5f3a5dc8310baf6a40c583645a709aa78
fares hamouda <hamoudaferes@gmail.com>
2022-03-13 17:40:50 +0100

    changing python spark job to java

--- Edge Side/spark/spark-job/spark-early-processing/src/main/java/SparkAppMain.java
+++ Edge Side/spark/spark-job/spark-early-processing/src/main/java/SparkAppMain.java
@@ -1,14 +1,27 @@
-import org.apache.spark.* ;
-import org.apache.spark.api.java.JavaSparkContext ;
+import org.apache.spark.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.streaming.*;
+import org.apache.spark.streaming.api.java.*;
+import scala.Tuple2;
+import org.apache.spark.streaming.mqtt.MQTTUtils;
 
 public class SparkAppMain {
-    public static void main(String[] args) throws IOException {
-        SparkConf sparkConf = new SparkConf()
-                .setAppName("Example Spark App")
-                .setMaster("local[*]") ; // Delete this line when submitting to a cluster
-        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-        JavaRDD<String> stringJavaRDD = sparkContext.textFile("/tmp/nationalparks.csv");
-        System.out.println("Number of lines in file = " + stringJavaRDD.count());
+
+    public void tojson(String message) {
+        System.out.println(message);
+    }
+    public static void main(String[] args)  {
+        SparkConf conf = new SparkConf().setMaster("").setAppName("sensors");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+        jssc.sparkContext().setLogLevel("ERROR");
+        String brokerUrl = "tcp://132.207.170.59:1883";
+
+        String topic = "topic";
+        JavaReceiverInputDStream<String> mqttStream = MQTTUtils.createStream(jssc, brokerUrl, topic);
+        JavaDStream<?> flightDetailsStream = mqttStream.map(x -> {
+
+            return x;
+        }); ;
 
     }
 }
