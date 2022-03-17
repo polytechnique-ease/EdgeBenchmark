a6cc26fb7f1e606d52e2c5cd3d43748048e9630b
fares hamouda <hamoudaferes@gmail.com>
2022-03-17 17:20:35 +0100

    maven

---
+++ Edge Side/spark/spark-job/maven-spark-processing/src/main/java/SparkMain.java
@@ -0,0 +1,31 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import java.io.IOException; 
+import org.apache.spark.streaming.mqtt.MQTTUtils;
+//import org.influxdb.InfluxDB;
+//import org.influxdb.InfluxDBFactory;
+/*import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;*/
+
+public class SparkMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     String influxDB = "ss";
+	        SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("sensors");
+	        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+	        jssc.sparkContext().setLogLevel("ERROR");
+	        String brokerUrl = "tcp://132.207.170.59:1883";
+
+	        String topic = "topic";
+	       // JavaReceiverInputDStream<String> mqttStream = MQTTUtils.createStream(jssc, brokerUrl, topic);
+
+	}
+
+}
