2367e6e7e1b60db4f6e858883589212f53a17377
fares hamouda <hamoudaferes@gmail.com>
2022-03-17 17:12:07 +0100

    maven

--- Edge Side/spark/spark-job/maven-spark-processing/src/main/java/SparkAppMain.java
+++ Edge Side/spark/spark-job/maven-spark-processing/src/main/java/SparkAppMain.java
@@ -5,39 +5,26 @@ import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import java.io.IOException; 
 import org.apache.spark.streaming.mqtt.MQTTUtils;
 //import org.influxdb.InfluxDB;
 //import org.influxdb.InfluxDBFactory;
-import org.json.JSONArray;
+/*import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.IOException ;
+import org.json.JSONObject;*/
 
 
 public class SparkAppMain {
     public static void main(String[] args) throws IOException {
         String influxDB = "ss";
-        SparkConf conf = new SparkConf().setAppName("sensors");
+        SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("sensors");
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
         jssc.sparkContext().setLogLevel("ERROR");
         String brokerUrl = "tcp://132.207.170.59:1883";
 
         String topic = "topic";
-        JavaReceiverInputDStream<String> mqttStream = MQTTUtils.createStream(jssc, brokerUrl, topic);
-        JavaDStream<JSONObject> sensorDetailsStream = mqttStream.map(x -> {
-            try {
-                return new JSONObject(x);
-            }catch (JSONException err){
-                System.out.println(err);
-            }
-            return null;
-        });
-            try {
-            jssc.start();
-            jssc.awaitTermination();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //JavaReceiverInputDStream<String> mqttStream = MQTTUtils.createStream(jssc, brokerUrl, topic);
+
     }
 
 }
