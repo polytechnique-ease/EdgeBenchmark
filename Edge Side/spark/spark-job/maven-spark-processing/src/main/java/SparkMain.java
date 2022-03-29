a12fe4f78db8ae21f10bf16d84c91b531dd87f79
fares hamouda <hamoudaferes@gmail.com>
2022-03-29 14:36:22 +0100

    changing the spark job

--- Edge Side/spark/spark-job/maven-spark-processing/src/main/java/SparkMain.java
+++ Edge Side/spark/spark-job/maven-spark-processing/src/main/java/SparkMain.java
@@ -55,9 +55,13 @@ public class SparkMain {
 			
 			SparkConf conf = new SparkConf().setAppName("sensors");
 			//conf.setMaster("spark://132.207.170.59:7077");
-		    conf.set("spark.executor.memory", "2000m");
-			JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+		    conf.set("spark.executor.memory", "2000m")
+			  .set("spark.memory.offHeap.enabled","true")
+				.set("spark.memory.offHeap.size","1g") ;
+
+		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
 			jssc.sparkContext().setLogLevel("WARN");
+
 	        String brokerUrl = "tcp://132.207.170.59:1883";
 			//jssc.checkpoint("checkpoint");
 	        String topic = "topic";
