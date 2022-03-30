cac261d44d348902f0a07030c4530644f363af64
fares hamouda <hamoudaferes@gmail.com>
2022-03-30 16:26:30 +0100

    changing the spark job

--- Edge Side/spark/spark-job/maven-spark-processing/src/main/java/SparkMain.java
+++ Edge Side/spark/spark-job/maven-spark-processing/src/main/java/SparkMain.java
@@ -68,7 +68,7 @@ public class SparkMain {
 
 
 		SparkConf conf = new SparkConf().setAppName("sensors");
-		conf.setMaster("spark://132.207.170.59:7077");
+		//conf.setMaster("spark://132.207.170.59:7077");
 		conf.set("spark.executor.memory", "1g");
 		conf.set("spark.driver.memory", "1g");
 		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
