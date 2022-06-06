da48deefcab9850aa4b08bd1561fdbc2355497cb
mahsahadian <mahsa_hadian@yahoo.com>
2022-06-06 10:24:06 -0400

    test

--- Sensor Side/temp/DataSender.py
+++ Sensor Side/temp/DataSender.py
@@ -3,5 +3,6 @@ import requests
 class DataSender:
 
     def sendData(self,json):
-    	requests.post('http://localhost:8088', json)
+    	requests.post('http://localhost:8089', json=json, headers = {'Content-type': 'application/json'}
+)
     	pass
